5a18095bc0a150abfbcf55da1c67f407aad26e9d
Doğancan <dogancanmavideniz@gmail.com>
2024-11-07 22:27:31 +0300

    Initial Commit

---
+++ server-driven-ui-example/server_driven_ui_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  server_driven_ui_exampleApp.swift
+//  server-driven-ui-example
+//
+//  Created by Doğancan Mavideniz on 7.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct server_driven_ui_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
